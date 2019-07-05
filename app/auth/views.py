041cc44de39db1a5039696020375f21a86ebcfcc
Omtatah <murtallahomtatah@gmail.com>
2019-07-05 16:06:50 +0300

    linked the views.py file to every dependancy needed to create views on the various templates

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,2 +1,6 @@
 from . import auth
 from .. import db
+from ..models import User
+from .forms import RegistrationForm,LoginForm
+from flask import render_template,redirect,url_for,flash,request
+from flask_login import login_user,login_required, logout_user, current_user
